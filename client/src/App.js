d584408ff966f6c34a082e7c8448e985f1fa39de
KimMinWoo <a41787192@gmail.com>
2021-03-11 12:18:04 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -11,83 +11,31 @@ import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
   root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: "auto"
+    marginTop: theme.spacing(2) * 3
   },
   table: {
     minWidth: 1080
   }
 });
 
-const customers = 
-[
-  {
-    "id": 1,
-    "image": 'https://placeimg.com/80/80/any/1',
-    "name": '블랙 위도우',
-    "birthday": '960421',
-    "gender": '여자',
-    "title": '어둠의 망자',
-    "job": '셀러리맨(히어로)'
-  },
-  {
-    "id": 2,
-    "image": 'https://placeimg.com/80/80/any/2',
-    "name": '캣우먼',
-    "birthday": '950110',
-    "gender": '여자',
-    "title": '블랙캣',
-    "job": '비서(히어로)'
-  },
-  {
-    "id": 3,
-    "image": 'https://placeimg.com/80/80/any/3',
-    "name": '할리 퀸즈',
-    "birthday": '970512',
-    "gender": '여자',
-    "title": '미치광이',
-    "job": '백수(히어로)'
-  },
-  {
-    "id": 4,
-    "image": 'https://placeimg.com/80/80/any/4',
-    "name": '토르',
-    "birthday": '931102',
-    "gender": '남자',
-    "title": '파괴의 신',
-    "job": '천둥군주(히어로)'
-  },
-  {
-    "id": 5,
-    "image": 'https://placeimg.com/80/80/any/5',
-    "name": '아이언맨',
-    "birthday": '930501',
-    "gender": '남자',
-    "title": '로봇맨',
-    "job": '셀러리맨(히어로)'
-  },
-  {
-    "id": 6,
-    "image": 'https://placeimg.com/80/80/any/6',
-    "name": '캡틴 아메리칸',
-    "birthday": '950424',
-    "gender": '남자',
-    "title": '어깨깡패',
-    "job": '군인(히어로)'
-  },
-  {
-    "id": 7,
-    "image": 'https://placeimg.com/80/80/any/7',
-    "name": '헐크',
-    "birthday": '950911',
-    "gender": '남자',
-    "title": '초록 괴물',
-    "job": '박사(히어로)'
-  },
-];
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const {classes} = this.props;
       return (
@@ -105,22 +53,8 @@ class App extends Component {
               </TableRow>
             </TableHead>
             <TableBody>
-                {
-                  customers.map(c => {
-                    return(
-                        <Customer
-                          key={c.id}
-                          id={c.id}
-                          image={c.image}
-                          name={c.name}
-                          birthday={c.birthday}
-                          gender={c.gender}
-                          title={c.title}
-                          job={c.job}
-                        />
-                      );
-                  })
-              }
+                {this.state.customers ? this.state.customers.map(c => {return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} title={c.title} job={c.job} />);
+              }) : ""}
             </TableBody>
           </Table>
         </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,72 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+          "id": 1,
+          "image": 'https://placeimg.com/80/80/any/1',
+          "name": '블랙 위도우',
+          "birthday": '960421',
+          "gender": '여자',
+          "title": '어둠의 망자',
+          "job": '셀러리맨(히어로)'
+        },
+        {
+          "id": 2,
+          "image": 'https://placeimg.com/80/80/any/2',
+          "name": '캣우먼',
+          "birthday": '950110',
+          "gender": '여자',
+          "title": '블랙캣',
+          "job": '비서(히어로)'
+        },
+        {
+          "id": 3,
+          "image": 'https://placeimg.com/80/80/any/3',
+          "name": '할리 퀸즈',
+          "birthday": '970512',
+          "gender": '여자',
+          "title": '미치광이',
+          "job": '백수(히어로)'
+        },
+        {
+          "id": 4,
+          "image": 'https://placeimg.com/80/80/any/4',
+          "name": '토르',
+          "birthday": '931102',
+          "gender": '남자',
+          "title": '파괴의 신',
+          "job": '천둥군주(히어로)'
+        },
+        {
+          "id": 5,
+          "image": 'https://placeimg.com/80/80/any/5',
+          "name": '아이언맨',
+          "birthday": '930501',
+          "gender": '남자',
+          "title": '로봇맨',
+          "job": '셀러리맨(히어로)'
+        },
+        {
+          "id": 6,
+          "image": 'https://placeimg.com/80/80/any/6',
+          "name": '캡틴 아메리칸',
+          "birthday": '950424',
+          "gender": '남자',
+          "title": '어깨깡패',
+          "job": '군인(히어로)'
+        },
+        {
+          "id": 7,
+          "image": 'https://placeimg.com/80/80/any/7',
+          "name": '헐크',
+          "birthday": '950911',
+          "gender": '남자',
+          "title": '초록 괴물',
+          "job": '박사(히어로)'
+        },
+      ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
