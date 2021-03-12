3fd8ca7281e4228d2be1cf43e12793656120fae2
KimMinWoo <a41787192@gmail.com>
2021-03-12 15:49:13 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -27,9 +28,22 @@ const styles = theme => ({
 
 class App extends Component {
 
-  state = {
-    customers: "",
-    completed: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
   }
 
   componentDidMount() {
@@ -80,6 +94,7 @@ class App extends Component {
               </TableBody>
             </Table>
           </Paper>
+          <CustomerAdd stateRefresh={this.stateRefresh} />
         </div>
       );
     }
